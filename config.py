1892df083a14c5809d52f95a673e6ec54f8829b5
bolt9923 <150222423+bolt9923@users.noreply.github.com>
2025-04-03 18:55:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config:
     API_HASH = "c81ae4c3dc026ea4bf49842a8ce4a5f9"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGO_URL = "mongodb+srv://supereme:supereme@supreme.pvfksri.mongodb.net/?retryWrites=true&w=majority&appName=supreme"
     START_PIC = "https://files.catbox.moe/ppvvg0.jpg"
     SUDOERS = filters.user(["6241051226"])
